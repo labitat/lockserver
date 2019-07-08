15bd3cc54caf669bbbf33f686ec3efedd82bed33
Emil Renner Berthing <esmil@labitat.dk>
2019-07-08 19:33:39 +0200

    lockd.py: some code cleanups
    Make sure we exit the program when the main thread
    breaks by marking threads with daemon=True.
    Now we don't need to explicitly exit the main thread
    since that's what happens anyway with unhandled exceptions.
    This should also make the exitcode != 0 so that
    systemd's Restart=on-failure will restart the script.
    
    Lastly we don't need to print timestamps now
    everything is logged to the journal (with a timestamp).

--- lockd.py
+++ lockd.py
@@ -14,17 +14,14 @@ def get_password():
 	with open('/home/doorman/lockserver.password', 'r') as f:
 		return f.readline().strip()
 
-webserver_password = get_password()
+WEBSERVER_PASSWORD = get_password()
 
 SEND_HASH_URL = "https://labitat.dk/member/money/doorputer_new_hash?{}"
 GET_DATA_URL  = "https://labitat.dk/member/money/doorputer_get_dates?{}"
 
 # number of seconds to wait between fetching
 # a new version of the user database
-web_update_interval = 60
-
-connection = sqlite.connect('/home/doorman/users.db')
-cursor = connection.cursor()
+WEB_UPDATE_INTERVAL = 60
 
 def ui(ser):
 	cur_mode = b"D"
@@ -44,25 +41,24 @@ def ui(ser):
 		button.close()
 		time.sleep(0.05)
 
-s = serial.Serial(
-	port	 = '/dev/ttyUSB0',
+ser = serial.Serial(
+	port     = '/dev/ttyUSB0',
 	baudrate = 9600,
 	bytesize = 8,
 	parity   = 'E',
 	stopbits = 2
 )
-threading.Thread(target=ui, args=(s,)).start()
+threading.Thread(target=ui, args=(ser,), daemon=True).start()
 
 def update_from_webserver():
 	con = sqlite.connect('/home/doorman/users.db')
 	cur = con.cursor()
+	params = urllib.parse.urlencode({'key': WEBSERVER_PASSWORD})
 
 	try:
-		params = urllib.parse.urlencode({'key': webserver_password})
 		members = None
 		with urllib.request.urlopen(GET_DATA_URL.format(params)) as d:
-			data = d.read().decode('utf-8')
-			members = json.loads(data)
+			members = json.loads(d.read().decode('utf-8'))
 
 		for member in members:
 			cur.execute(
@@ -102,77 +98,61 @@ def update_from_webserver():
 		con.commit()
 		return True
 	except:
-		sys.stderr.write("\n{}\n".format(datetime.now()))
 		traceback.print_exc(file=sys.stderr)
 		return False
 
 def periodic_updater():
 	while True:
-		try:
-			update_from_webserver()
-			time.sleep(web_update_interval)
-		except:
-			sys.stderr.write("\n{}\n".format(datetime.now()))
-			traceback.print_exc(file=sys.stderr)
+		update_from_webserver()
+		time.sleep(WEB_UPDATE_INTERVAL)
 
-threading.Thread(target=periodic_updater).start()
+threading.Thread(target=periodic_updater, daemon=True).start()
 
 def send_to_webserver(hash):
+	params = urllib.parse.urlencode({
+		'key':  WEBSERVER_PASSWORD,
+		'hash': hash
+	})
 	try:
-		params = urllib.parse.urlencode({
-			'key':  webserver_password,
-			'hash': hash
-		})
-		# 202 means that the same unknown hash was
-		# received twice before a timeout which allows
-		# a website-user to claim the hash as their own
-		print("sending to webserver")
 		with urllib.request.urlopen(SEND_HASH_URL.format(params)) as d:
-			if d.getcode() == 202:
-				return True
-			else:
-				return False
+			# 202 means that the same unknown hash was
+			# received twice before a timeout which allows
+			# a website-user to claim the hash as their own
+			return d.getcode() == 202
 	except:
-		sys.stderr.write("\n{}\n".format(datetime.now()))
 		traceback.print_exc(file=sys.stderr)
 		return False
 
+con = sqlite.connect('/home/doorman/users.db')
+cur = con.cursor()
+
 while True:
-	try:
-		data = s.readline()
-		data = data[:-1]
+	data = ser.readline()
+	data = data[:-1]
 
-		if data[:5] != b"ALIVE":
-			print("DEBUG: {}".format(data))
+	if data[:5] != b"ALIVE":
+		print("DEBUG: {}".format(data))
 
-		if data[:5] == b"HASH+":
-			h = data[5:45].decode('utf-8')
-			cursor.execute(
-				"SELECT 1 "
-				"FROM hashes "
-				"WHERE hash = ? AND expires >= ?", [
-					h,
-					datetime.now().strftime('%Y-%m-%d')
-				]
-			)
-			if cursor.fetchone() != None:
-				s.write(b"O")
-				print("Opening")
+	if data[:5] == b"HASH+":
+		h = data[5:45].decode('utf-8')
+		cur.execute(
+			"SELECT 1 "
+			"FROM hashes "
+			"WHERE hash = ? AND expires >= ?", [
+				h,
+				datetime.now().strftime('%Y-%m-%d')
+			]
+		)
+		if cur.fetchone() != None:
+			ser.write(b"O")
+			print("Opening")
+		else:
+			print("Sending hash to webserver")
+			if send_to_webserver(h):
+				ser.write(b"V")
+				print("Validated")
 			else:
-				if send_to_webserver(h):
-					s.write(b"V")
-					print("Validated")
-				else:
-					s.write(b"R")
-					print("Rejected")
-	except:
-		# print exception
-		sys.stderr.write("\n{}\n".format(datetime.now()))
-		traceback.print_exc(file=sys.stderr)
-		# Sometimes we loose the serial connection here.
-		# Rather than try to coordinate serial port reopen across
-		# all the threads, just exit, and let parent init script
-		# restart us.
-		sys.exit()
+				ser.write(b"R")
+				print("Rejected")
 
 # vim: set ts=4 sw=4 noet:
